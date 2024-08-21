103a155b15a52651ed54e31f5cbf22ee736f6da4
Taufiq Hidayatulloh <hidayatullohtaufiq0@gmail.com>
2024-08-21 12:00:08 +0700

    adding assets

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,17 @@ module.exports = {
   theme: {
     extend: {
       colors: {
-        primary: '#640EF1',
+        'primary-500': '#640EF1',
+        'accent-900': '#080C2E',
+        'accent-1000': '#070C29',
+        'surface-50': '#F1F4F5',
+        'surface-100': '#8D90A8',
+        'surface-200': '#5D5D7C',
+        'surface-3000': '#424560',
+        'secondary': '#FF961A',
+        'warning': '#FFD15A',
+        'danger': '#FF3232',
+        'shade': '#070C29',
       },
       fontWeight: {
         'extralight': '200',
